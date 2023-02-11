d210cb8a1f18a338f3557f846adf5dad9b3d6ffa
Habibmahaasin <habibirf23@gmail.com>
2023-02-12 01:21:44 +0700

    Revert "update routing"
    This reverts commit 7245196c816f62e6fb52094ffb795c78be859e7e.

--- sql/gop-api.sql
+++ sql/gop-api.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Waktu pembuatan: 11 Feb 2023 pada 18.07
+-- Waktu pembuatan: 11 Feb 2023 pada 15.09
 -- Versi server: 5.7.39
 -- Versi PHP: 7.4.33
 
@@ -42,7 +42,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_uuid`, `name`, `email`, `password`, `date_created`, `date_updated`) VALUES
-(1, '56e1c9c6-a9e9-11ed-8350-60599c074d6b', 'Habib Irfan Mahaasin', 'admin@mahaasin.com', '$2a$10$Qyi2SwAX6HyqPe/RVTJeUOe0x0CjrIxBtBrs0k5SOxeKMa8KnzxZG', '2023-02-11 15:35:22.000', '2023-02-12 01:06:46.669'),
+(1, '56e1c9c6-a9e9-11ed-8350-60599c074d6b', 'mahaasin', 'mahaasin@gmail.com', '$2a$10$k364biWokQMSOsDAif7YFeessVYy6P7uST33oamPpKTQdwqSx4TJm', '2023-02-11 15:35:22.000', '2023-02-11 21:54:42.973'),
+(2, 'db974aaa-c38a-40c5-8d51-d76af5e39e23', 'mahaasin', 'habibirf23@gmail.com', '$2a$10$Hmjg6lnCWU3YAQZ0ww1RTef0t3PKAffAs0DnqcpfLffKW2y8Axxbq', '2023-02-11 16:51:56.112', '2023-02-11 16:51:56.112'),
+(3, 'bd44dbd4-2c3a-447f-b71f-9a3a5d66af30', 'Pengelola', 'pengelola@mahaasin.com', '$2a$10$0.tEKRVwUnFZuHh8wLTV7uawtA6ihtCc20LcqHpiOXJx8P1hOrFSe', '2023-02-11 16:54:04.644', '2023-02-11 16:54:04.644'),
 (5, 'a38f5510-7dcf-412c-bfcc-6b47c6cb1bc8', 'Super Admin', 'superadmin@gmail.com', '$2a$10$pgjAffXeyEhNwgZK004keOctCHbaTJOAKgTUL4Kt2lNhQbvL6wxHu', '2023-02-11 19:57:34.602', '2023-02-11 19:57:34.602');
 
 --
