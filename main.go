2aa22df25f2dc7e51558d3dfe4909b4eb91549a5
Habibmahaasin <habibirf23@gmail.com>
2023-02-17 03:02:20 +0700

    TESTLAGI

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gop-api/app/config"
+	"gop-api/app/database"
+	"gop-api/routes"
+	"log"
+	"os"
+)
 
 func main() {
-	// conf, err := config.Init()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// db := database.Init(conf)
-	// router := routes.Init(db)
-	router := gin.Default()
-	router.Run(":8080")
+	conf, err := config.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := database.Init(conf)
+	router := routes.Init(db)
+
+	router.Run(":" + os.Getenv("PORT"))
 }
